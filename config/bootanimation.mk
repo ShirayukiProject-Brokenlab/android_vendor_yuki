61cdf02de5abe616d3335a251f1536bd3aa551a9
Raka Prasetya Pratama <123168129+Hirozuto@users.noreply.github.com>
2023-05-23 20:38:43 +0800

    config: use yukiprjkt bootanimation by default

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -2,14 +2,14 @@
 
 # 1080p
 ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
-PRODUCT_COPY_FILES += vendor/yuki/prebuilt/common/bootanimation/1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+PRODUCT_COPY_FILES += vendor/yuki/prebuilt/common/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 # 720p
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
-PRODUCT_COPY_FILES += vendor/yuki/prebuilt/common/bootanimation/720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-# 480p
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),480)
-PRODUCT_COPY_FILES += vendor/yuki/prebuilt/common/bootanimation/480.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+PRODUCT_COPY_FILES += vendor/yuki/prebuilt/common/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+# 1440
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+PRODUCT_COPY_FILES += vendor/yuki/prebuilt/common/bootanimation/bootanimation_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 # Default to 720p if the device does not set the flag.
 else
-PRODUCT_COPY_FILES += vendor/yuki/prebuilt/common/bootanimation/720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+PRODUCT_COPY_FILES += vendor/yuki/prebuilt/common/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 endif
